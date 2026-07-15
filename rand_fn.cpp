d9a0121622fffcebfbcb2963bc293e981d6df9cc
digikar99 <digikar@proton.me>
2026-07-15 15:41:17 +0200

    [int] shorten rand_fn.cpp using preprocessor macro

--- rand_fn.cpp
+++ rand_fn.cpp
@@ -1,4 +1,20 @@
 
+// Strips the outer parens off a tuple: CEIGEN_LITE_ARGS (a, b) -> a, b
+#define CEIGEN_LITE_ARGS(...) __VA_ARGS__
+
+#define CEIGEN_LITE_DEFINE_DIST(CTYPE, SUF, DIST, PARAMS, ARGVALS)	\
+  void CEIGEN_LITE_##SUF##DIST##_r(CEIGEN_LITE_rng *urng64, const long n, \
+				   CTYPE *x, CEIGEN_LITE_ARGS PARAMS) {	\
+    Map<MatrixX<CTYPE>> eigmat(x, n, 1);				\
+    eigmat = Rand::DIST<MatrixX<CTYPE>>(n, 1, urng64->engine,		\
+					CEIGEN_LITE_ARGS ARGVALS);	\
+  }									\
+  void CEIGEN_LITE_##SUF##DIST(const long n, CTYPE *x,			\
+			       CEIGEN_LITE_ARGS PARAMS) {		\
+    CEIGEN_LITE_##SUF##DIST##_r(&global_urng64, n, x,			\
+				CEIGEN_LITE_ARGS ARGVALS);		\
+  }
+
 extern "C"{
   struct CEIGEN_LITE_rng { Rand::Vmt19937_64 engine; };
   CEIGEN_LITE_rng global_urng64{Rand::Vmt19937_64{42}};
@@ -11,48 +27,10 @@ extern "C"{
     global_urng64.engine = Rand::Vmt19937_64(seed);
   }
 
-  void CEIGEN_LITE_snormal_r(CEIGEN_LITE_rng* urng64,
-			     const long n, float *x, float mean, float stdev){
-    Map<MatrixX<float>> eigmat(x, n, 1);
-    eigmat = Rand::normal<MatrixX<float>>(n, 1, urng64->engine, mean, stdev);
-  }
-  void CEIGEN_LITE_snormal(const long n, float *x, float mean, float stdev){
-    CEIGEN_LITE_snormal_r(&global_urng64, n, x, mean, stdev);
-  }
-
-  void CEIGEN_LITE_dnormal_r(CEIGEN_LITE_rng* urng64,
-			     const long n, double *x, double mean, double stdev){
-    Map<MatrixX<double>> eigmat(x, n, 1);
-    eigmat = Rand::normal<MatrixX<double>>(n, 1, urng64->engine, mean, stdev);
-  }
-  void CEIGEN_LITE_dnormal(const long n, double *x, double mean, double stdev){
-    CEIGEN_LITE_dnormal_r(&global_urng64, n, x, mean, stdev);
-  }
-
-
-  void CEIGEN_LITE_sbeta_r(CEIGEN_LITE_rng* urng64,
-			   const long n, float *x, float a, float b){
-    Map<MatrixX<float>> eigmat(x, n, 1);
-    eigmat = Rand::beta<MatrixX<float>>(n, 1, urng64->engine, a, b);
-    return;
-  }
-  void CEIGEN_LITE_dbeta_r(CEIGEN_LITE_rng* urng64,
-			   const long n, double *x, double a, double b){
-    Map<MatrixX<double>> eigmat(x, n, 1);
-    eigmat = Rand::beta<MatrixX<double>>(n, 1, urng64->engine, a, b);
-    return;
-  }
-
-  void CEIGEN_LITE_schiSquared_r(CEIGEN_LITE_rng* urng64,
-				 const long n, float *x, float ndof){
-    Map<MatrixX<float>> eigmat(x, n, 1);
-    eigmat = Rand::chiSquared<MatrixX<float>>(n, 1, urng64->engine, ndof);
-    return;
-  }
-  void CEIGEN_LITE_dchiSquared_r(CEIGEN_LITE_rng* urng64,
-				 const long n, double *x, double ndof){
-    Map<MatrixX<double>> eigmat(x, n, 1);
-    eigmat = Rand::chiSquared<MatrixX<double>>(n, 1, urng64->engine, ndof);
-    return;
-  }
-};
+  CEIGEN_LITE_DEFINE_DIST(float,  s, normal, (float mean, float stdev), (mean, stdev));
+  CEIGEN_LITE_DEFINE_DIST(double, d, normal, (double mean, double stdev), (mean, stdev));
+  CEIGEN_LITE_DEFINE_DIST(float,  s, beta,   (float a, float b), (a, b));
+  CEIGEN_LITE_DEFINE_DIST(double, d, beta,   (double a, double b), (a, b));
+  CEIGEN_LITE_DEFINE_DIST(float,  s, chiSquared, (float ndof), (ndof));
+  CEIGEN_LITE_DEFINE_DIST(double, d, chiSquared, (double ndof), (ndof));
+}
